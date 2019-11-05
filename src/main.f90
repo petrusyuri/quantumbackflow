1c0892742cfb354a0345b087f696628e0a2cc23f
Petrus Yuri <petrus.yuri@gmail.com>
2019-11-05 15:13:45 -0300

    Update main.f90

--- src/main.f90
+++ src/main.f90
@@ -2,7 +2,7 @@ program jump
 
     use jump_integration
     use eispack
-    
+
     implicit none
 
     real (kind = 8) interval, x_test
@@ -29,33 +29,75 @@ program jump
     integer         :: n_args, iarg
     character(len=16), dimension(7) :: args
 
-    ! Find out how many arguments have been provided. If it's not 6, abort
+    character(8)  :: date
+    character(10) :: time
+    character(21) :: datetime
+
+    character(255) :: logfile
+    character(255) :: datafile
+    character(255) :: gpltfile
+    character(255) :: plotfile
+    character(255) :: cwd
+
+    call date_and_time(date = date, time = time)
+    call getcwd(cwd)
+
+    datetime = date(1:4)//"-"//date(5:6)//"-"//date(7:8)
+    datetime = trim(datetime)//"_("//time(1:2)//"-"//time(3:4)//"-"//time(5:6)//")"
+
+    logfile  = trim(cwd)//"\out\log_"//trim(datetime)//".txt"
+    datafile = trim(cwd)//"\out\data_"//trim(datetime)//".csv"
+    gpltfile = trim(cwd)//"\out\gplt_"//trim(datetime)//".plt"
+    plotfile = trim(cwd)//"\out\plot_"//trim(datetime)//".png"
+
+    write(*,'(a)') "********************"
+    write(*,'(a)') "JUMP MODEL TEST"
+    write(*,'(a)') "********************"
+    write(*,*) ""
+
+    ! Find out how many arguments have been provided. If it's not 7, warning
     n_args = command_argument_count()
     if (n_args /= 7) then
-      if (n_args > 0) write(*,'(a,I3)') , "ERROR: Expected 7 arguments, but got", n_args
-      write(*,*) , "Usage:"
-      write(*,*) , "  ./backflow <n> <p_cutoff> <alpha> <x_low> <x_high> <x_steps> <fun_type>"
-      write(*,*) , ""
-      write(*,*) , "Example:"
-      write(*,*) , "  ./backflow 1000 200.0 -4.0 -1.0 1.0 100 1"
-      stop
+      write(*,'(a,I3)') "WARNING: Expected 7 arguments, but got", n_args
+      write(*,*) ""
+      write(*,*) "Usage:"
+      write(*,*) "  ./backflow <n> <p_cutoff> <alpha> <x_low> <x_high> <x_steps> <fun_type>"
+      write(*,*) ""
+      write(*,*) "Example:"
+      write(*,*) "  ./backflow 1000 200.0 0.1 -1.0 1.0 100 1"
+      write(*,*) ""
+      write(*,*) "Using default parameters"
+      write(*,*) ""
+
+      MODEL_N          =  200
+      MODEL_P_CUTOFF   =  100.0D0
+      MODEL_ALPHA      =    0.1D0
+      MODEL_X_LOW      = -  0.5D0
+      MODEL_X_HIGH     =    0.5D0
+      MODEL_X_STEPS    =    30
+      MODEL_FUN_TYPE   =    1
+    else
+        ! If we got the right number of arguments, put them into an array of characters
+        do iarg=1, n_args
+          call get_command_argument(iarg, args(iarg))
+        end do
+
+        ! Read from the array of arguments into the appropriate variables
+        read(args(1), *) MODEL_N
+        read(args(2), *) MODEL_P_CUTOFF
+        read(args(3), *) MODEL_ALPHA
+        read(args(4), *) MODEL_X_LOW
+        read(args(5), *) MODEL_X_HIGH
+        read(args(6), *) MODEL_X_STEPS
+        read(args(7), *) MODEL_FUN_TYPE
     end if
 
-    ! If we got the right number of arguments, put them into an array of characters
-    do iarg=1, n_args
-      call get_command_argument(iarg, args(iarg))
-    end do
-
-    ! Read from the array of arguments into the appropriate variables
-    read(args(1), *) MODEL_N
-    read(args(2), *) MODEL_P_CUTOFF
-    read(args(3), *) MODEL_ALPHA
-    read(args(4), *) MODEL_X_LOW
-    read(args(5), *) MODEL_X_HIGH
-    read(args(6), *) MODEL_X_STEPS
-    read(args(7), *) MODEL_FUN_TYPE
+    open(1, file = logfile, status = 'new')
+    open(2, file = datafile, status = 'new')
 
     ! Write some information about the parameters being used this run
+    write(*,'(a)') "********************"
+    write(*,'(a)') ""
     write(*,'(a)') "Running with parameters:"
     write(*,'(a,I7)')    "  MODEL_N        = ", MODEL_N
     write(*,'(a,F12.4)') "  MODEL_P_CUTOFF = ", MODEL_P_CUTOFF
@@ -64,7 +106,24 @@ program jump
     write(*,'(a,F12.4)') "  MODEL_X_HIGH   = ", MODEL_X_HIGH
     write(*,'(a,I7)')    "  MODEL_X_STEPS  = ", MODEL_X_STEPS
     write(*,'(a,I7)')    "  FUN_TYPE       = ", MODEL_FUN_TYPE
-    write(*,*) , ""
+    write(*,'(a)') ""
+    write(*,'(a)') "********************"
+    write(*,*) ""
+
+    ! Write some information about the parameters being used this run
+    write(1,'(a)') "********************"
+    write(1,'(a)') ""
+    write(1,'(a)') "Running with parameters:"
+    write(1,'(a,I7)')    "  MODEL_N        = ", MODEL_N
+    write(1,'(a,F12.4)') "  MODEL_P_CUTOFF = ", MODEL_P_CUTOFF
+    write(1,'(a,F12.4)') "  MODEL_ALPHA    = ", MODEL_ALPHA
+    write(1,'(a,F12.4)') "  MODEL_X_LOW    = ", MODEL_X_LOW
+    write(1,'(a,F12.4)') "  MODEL_X_HIGH   = ", MODEL_X_HIGH
+    write(1,'(a,I7)')    "  MODEL_X_STEPS  = ", MODEL_X_STEPS
+    write(1,'(a,I7)')    "  FUN_TYPE       = ", MODEL_FUN_TYPE
+    write(1,'(a)') ""
+    write(1,'(a)') "********************"
+    write(1,*) ""
 
     allocate(points(MODEL_X_STEPS + 1))
     allocate(realMatrix(MODEL_N, MODEL_N))
@@ -76,14 +135,25 @@ program jump
 
     interval = (MODEL_X_HIGH - (MODEL_X_LOW)) / MODEL_X_STEPS
 
-    call JumpModel(MODEL_N, MODEL_P_CUTOFF, MODEL_ALPHA, MODEL_FUN_TYPE)
+    call JumpModel(MODEL_N, MODEL_P_CUTOFF, MODEL_FUN_TYPE)
 
-    write (*,fmt = "(A)") "POINTS = ["
+    write(*,'(a,F12.4)') "backflow1 (bf1): MODEL_ALPHA = ", - MODEL_ALPHA
+    write(*,'(a,F12.4)') "backflow2 (bf2): MODEL_ALPHA = ", MODEL_ALPHA
+    write(*,*) ""
+
+    write(1,'(a,F12.4)') "backflow1 (bf1): MODEL_ALPHA = ", - MODEL_ALPHA
+    write(1,'(a,F12.4)') "backflow2 (bf2): MODEL_ALPHA = ", MODEL_ALPHA
+    write(1,*) ""
+
 
     do test = 0, MODEL_X_STEPS
         x_test = MODEL_X_LOW + test*interval
 
-        write(*, fmt = '(F10.3, A)', advance = "no") x_test, ", "
+        write(*, fmt = '(A, F6.2, A)', advance = "no") "x = ", x_test, ", bf1 = "
+        write(1, fmt = '(A, F6.2, A)', advance = "no") "x = ", x_test, ", bf1 = "
+        write(2, fmt = '(F6.2, A)', advance = "no") x_test, "; "
+
+        call SetAlpha(- MODEL_ALPHA)
 
         call GaussianCurrentKernel(0.1D0, x_test)
 
@@ -100,16 +170,60 @@ program jump
 
         call ch(MODEL_N, realMatrix, imagMatrix, w, .FALSE., zr,zi,ierr)
 
-        points(test + 1) = w(1)
+        write(*, fmt = '(F15.12, A)', advance = "no") w(1), ", bf2 = "
+        write(1, fmt = '(F15.12, A)', advance = "no") w(1), ", bf2 = "
+        write(2, fmt = '(F15.12, A)', advance = "no") w(1), "; "
 
-        write(*, fmt = '(F18.12, A)') w(1), ";"
+        call SetAlpha(MODEL_ALPHA)
+
+        call GaussianCurrentKernel(0.1D0, x_test)
+
+        do row = 1, MODEL_N
+            do col = 1, row
+                tempVar = BasisMatrixElement(row - 1, col - 1)
+
+                realMatrix(row, col) = DREAL(tempVar)
+                imagMatrix(row, col) = DIMAG(tempVar)
+            end do
+
+!            print*, "Row = ", row
+        end do
+
+        call ch(MODEL_N, realMatrix, imagMatrix, w, .FALSE., zr,zi,ierr)
+
+        write(*, fmt = '(F15.12)') w(1)
+        write(1, fmt = '(F15.12)') w(1)
+        write(2, fmt = '(F15.12)') w(1)
 
     end do
 
-    write (*,*) "];"
+    open(3, file = gpltfile, status = 'new')
 
-    write (*,*)
+    write(3,'(a)') "set encoding utf8"
+    write(3,'(a)') "set terminal png size 960, 720 enhanced"
+    write(3,'(a)') "set output '"//trim(plotfile)//"'"
+    write(3,'(a)') "set datafile separator ';'"
+    write(3,'(a)') ""
+    write(3,'(a)', advance = "no") "set title 'Backflow in jump defect: "
+    write(3,'(a, i4, a, F6.1, a, F8.2)') "N = ", MODEL_N, ", P_{cutoff} = ", MODEL_P_CUTOFF, ", |{/Symbol a}| = ", MODEL_ALPHA
+    write(3,'(a)') "set style line 1 linecolor rgb 'red' linewidth 2"
+    write(3,'(a)') "set style line 2 linecolor rgb 'blue' linewidth 2"
+    write(3,'(a)') "set style line 3 linecolor rgb 'forest-green' linewidth 2"
+    write(3,'(a)') ""
+    write(3,'(a)') "set label ''"
+    write(3,'(a)') "set xlabel 'x_{0}'"
+    write(3,'(a)') "set ylabel '{/Symbol b}_V(f)'"
+    write(3,'(a)') "set key center right"
+    write(3,'(a)') ""
+    write(3,'(a)') "set yrange [*:0]"
+    write(3,'(a)') ""
+    write(3,'(a)') "plot '"//trim(datafile)//"' using 1:2 with lines ls 1 title '{/Symbol a} < 0', \"
+    write(3,'(a)') "     '"//trim(datafile)//"' using 1:3 with lines ls 2 title '{/Symbol a} > 0'"
 
-    write (*,*) "plot(POINTS(:,1), POINTS(:,2));"
+    close(1)
+    close(2)
+    close(3)
+
+    call system("gnuplot -p "//trim(gpltfile))
 
 end program
